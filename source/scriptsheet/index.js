27c333028cbda5ad5d25d7cb61a21c5ed4a86280
Akshaibiju <92768719+Akshaibiju@users.noreply.github.com>
2023-03-06 16:32:18 +0530

    main-responsive

--- source/scriptsheet/index.js
+++ source/scriptsheet/index.js
@@ -1,1 +1,48 @@
 console.log("Online");
+
+let brandone = document.getElementsByClassName("brand-one")[0]
+brandone.addEventListener("click", () => {
+    window.location.href = "#home";
+});
+
+let github = document.getElementsByClassName("gh")[0]
+github.addEventListener("click", () => {
+    window.open("http://github.com/Akshaibiju/");
+});
+
+let fiverr = document.getElementsByClassName("fr")[0]
+fiverr.addEventListener("click", () => {
+    window.open("https://www.fiverr.com/users/akshaibiju286/");
+});
+
+let upwork = document.getElementsByClassName("uw")[0]
+upwork.addEventListener("click", () => {
+    window.open("https://www.upwork.com/freelancers/~0184babb6cd03f5076");
+});
+
+let freelancer = document.getElementsByClassName("fl")[0]
+freelancer.addEventListener("click", () => {
+    window.open("https://www.freelancer.com/u/akshaibiju732");
+});
+
+let fiver = document.getElementsByClassName("fiverr")[0]
+fiver.addEventListener("click", () => {
+    window.open("https://www.fiverr.com/users/akshaibiju286/");
+});
+
+let uw = document.getElementsByClassName("upwork")[0]
+uw.addEventListener("click", () => {
+    window.open("https://www.upwork.com/freelancers/~0184babb6cd03f5076");
+});
+let twitter = document.getElementsByClassName("twitter")[0]
+twitter.addEventListener("click", () => {
+    window.open("https://twitter.com/REDTECHWOLF1");
+})
+let facebook = document.getElementsByClassName("facebook")[0]
+facebook.addEventListener("click", () => {
+    window.open("https://www.facebook.com/Akshai.0.0");
+})
+let ghub = document.getElementById("githubb");
+ghub.addEventListener("click", () => {
+    window.open("https://github.com/Akshaibiju/");
+})
